fd07cd146ca7be06387eb79f9cd83be5cd8b76c9
Sarah Chisholm <89938738+sarah-chisholm@users.noreply.github.com>
2025-04-24 14:01:00 -0400

    Update package for SyncroSim v3.1

--- src/burnProbability.R
+++ src/burnProbability.R
@@ -24,7 +24,7 @@ checkPackageVersion <- function(packageString, minimumVersion){
   }
 }
 
-checkPackageVersion("rsyncrosim", "2.0.0")
+checkPackageVersion("rsyncrosim", "2.1.0")
 checkPackageVersion("tidyverse",  "2.0.0")
 checkPackageVersion("dplyr",      "1.1.2")
 checkPackageVersion("codetools",  "0.2.19")
